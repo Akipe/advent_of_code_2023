40b33089eba9a0338d19d4a5b2b6e513708f721b
Julien Milletre Akipe <code.julien@milletre.fr>
2023-12-01 20:51:43 +0100

    Add Day 01 part 1

---
+++ php/src/Day01Part1/calibration.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Akipe\AdventOfCode2023\Day01Part1;
+
+use Akipe\AdventOfCode2023\Utils\File;
+use Exception;
+
+require_once '../../vendor/autoload.php';
+
+define("CALIBRATION_INPUT_EXAMPLE", "./input_example.txt");
+define("CALIBRATION_INPUT_PATH", "./input.txt");
+
+function getFirstDigit(string $line): int|false
+{
+  $digitLine = getOnlyDigit($line);
+
+  if (strlen($digitLine) < 1) {
+    return false;
+  }
+
+  return $digitLine[0];
+}
+
+function getLastDigit(string $line): int|false
+{
+  $digitLine = getOnlyDigit($line);
+
+  if (strlen($digitLine) < 1) {
+    return false;
+  }
+
+  $positionLastDigit = strlen($digitLine) - 1;
+
+  return $digitLine[$positionLastDigit];
+}
+
+function getOnlyDigit(string $line): string
+{
+  return filter_var($line, FILTER_SANITIZE_NUMBER_INT);
+}
+
+function getCalibrationValue(File $calibrationInput): int
+{
+  $calibrationValue = 0;
+
+  foreach($calibrationInput as $lineCalibration) {
+    $digits = "";
+    $digits .= getFirstDigit($lineCalibration);
+    $digits .= getLastDigit($lineCalibration);
+    $calibrationValue += (int) $digits;
+  }
+
+  return $calibrationValue;
+}
+
+function main() {
+  try {
+
+    // $calibrationInput = new File(CALIBRATION_INPUT_EXAMPLE); # Result :142
+    $calibrationInput = new File(CALIBRATION_INPUT_PATH); # Result : 55488
+    echo getCalibrationValue($calibrationInput);
+
+  } catch(Exception $exception) {
+    echo "Error : ". $exception->getMessage();
+  }
+}
+
+main();
---
+++ php/src/Utils/File.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Akipe\AdventOfCode2023\Utils;
+
+use Iterator;
+
+class File implements Iterator
+{
+  private array $lines;
+  private string $content;
+  private int $index;
+
+  public function __construct(
+    private string $path,
+  ) {
+    $this->setContent();
+    $this->setLines();
+    $this->index = 0;
+  }
+
+  private function setContent(): void
+  {
+    $content = file_get_contents($this->path);
+
+    if (!$content) {
+      throw new \ErrorException("File at path \"". $this->path ."\" can't be open!");
+    }
+
+    $this->content = $content;
+  }
+
+  public function getContent(): string
+  {
+    return $this->content;
+  }
+
+  public function setLines(): void
+  {
+    $this->lines = explode(PHP_EOL, $this->content);
+  }
+
+  public function current(): mixed
+  {
+    return $this->lines[$this->index];
+  }
+
+  public function next(): void
+  {
+    $this->index++;
+  }
+
+  public function key(): mixed
+  {
+    return $this->index;
+  }
+
+  public function valid(): bool
+  {
+    return $this->index < count($this->lines);
+  }
+
+  public function rewind(): void
+  {
+    $this->index = 0;
+  }
+}
