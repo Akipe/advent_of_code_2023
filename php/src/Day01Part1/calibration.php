c47c0b4cb1bd5fc2f656ec8e6486f1be1181b6a7
Julien Milletre Akipe <code.julien@milletre.fr>
2023-12-01 20:59:32 +0100

    Apply PHP Coding Standards Fixer (PHP-CS-Fixer)

--- php/src/Day01Part1/calibration.php
+++ php/src/Day01Part1/calibration.php
@@ -12,57 +12,58 @@ define("CALIBRATION_INPUT_PATH", "./input.txt");
 
 function getFirstDigit(string $line): int|false
 {
-  $digitLine = getOnlyDigit($line);
+    $digitLine = getOnlyDigit($line);
 
-  if (strlen($digitLine) < 1) {
-    return false;
-  }
+    if (strlen($digitLine) < 1) {
+        return false;
+    }
 
-  return $digitLine[0];
+    return $digitLine[0];
 }
 
 function getLastDigit(string $line): int|false
 {
-  $digitLine = getOnlyDigit($line);
+    $digitLine = getOnlyDigit($line);
 
-  if (strlen($digitLine) < 1) {
-    return false;
-  }
+    if (strlen($digitLine) < 1) {
+        return false;
+    }
 
-  $positionLastDigit = strlen($digitLine) - 1;
+    $positionLastDigit = strlen($digitLine) - 1;
 
-  return $digitLine[$positionLastDigit];
+    return $digitLine[$positionLastDigit];
 }
 
 function getOnlyDigit(string $line): string
 {
-  return filter_var($line, FILTER_SANITIZE_NUMBER_INT);
+    return filter_var($line, FILTER_SANITIZE_NUMBER_INT);
 }
 
 function getCalibrationValue(File $calibrationInput): int
 {
-  $calibrationValue = 0;
+    $calibrationValue = 0;
 
-  foreach($calibrationInput as $lineCalibration) {
-    $digits = "";
-    $digits .= getFirstDigit($lineCalibration);
-    $digits .= getLastDigit($lineCalibration);
-    $calibrationValue += (int) $digits;
-  }
+    foreach($calibrationInput as $lineCalibration) {
+        $digits = "";
+        $digits .= getFirstDigit($lineCalibration);
+        $digits .= getLastDigit($lineCalibration);
+        $calibrationValue += (int) $digits;
+    }
 
-  return $calibrationValue;
+    return $calibrationValue;
 }
 
-function main() {
-  try {
+function main()
+{
+    try {
 
-    // $calibrationInput = new File(CALIBRATION_INPUT_EXAMPLE); # Result :142
-    $calibrationInput = new File(CALIBRATION_INPUT_PATH); # Result : 55488
-    echo getCalibrationValue($calibrationInput);
+        // $calibrationInput = new File(CALIBRATION_INPUT_EXAMPLE); # Result :142
+        $calibrationInput = new File(CALIBRATION_INPUT_PATH); # Result : 55488
+        echo getCalibrationValue($calibrationInput);
 
-  } catch(Exception $exception) {
-    echo "Error : ". $exception->getMessage();
-  }
+    } catch(Exception $exception) {
+        echo "Error : ". $exception->getMessage();
+    }
 }
 
 main();
--- php/src/Utils/File.php
+++ php/src/Utils/File.php
@@ -6,61 +6,61 @@ use Iterator;
 
 class File implements Iterator
 {
-  private array $lines;
-  private string $content;
-  private int $index;
+    private array $lines;
+    private string $content;
+    private int $index;
 
-  public function __construct(
-    private string $path,
-  ) {
-    $this->setContent();
-    $this->setLines();
-    $this->index = 0;
-  }
-
-  private function setContent(): void
-  {
-    $content = file_get_contents($this->path);
-
-    if (!$content) {
-      throw new \ErrorException("File at path \"". $this->path ."\" can't be open!");
+    public function __construct(
+        private string $path,
+    ) {
+        $this->setContent();
+        $this->setLines();
+        $this->index = 0;
     }
 
-    $this->content = $content;
-  }
+    private function setContent(): void
+    {
+        $content = file_get_contents($this->path);
 
-  public function getContent(): string
-  {
-    return $this->content;
-  }
+        if (!$content) {
+            throw new \ErrorException("File at path \"". $this->path ."\" can't be open!");
+        }
 
-  public function setLines(): void
-  {
-    $this->lines = explode(PHP_EOL, $this->content);
-  }
+        $this->content = $content;
+    }
 
-  public function current(): mixed
-  {
-    return $this->lines[$this->index];
-  }
+    public function getContent(): string
+    {
+        return $this->content;
+    }
 
-  public function next(): void
-  {
-    $this->index++;
-  }
+    public function setLines(): void
+    {
+        $this->lines = explode(PHP_EOL, $this->content);
+    }
 
-  public function key(): mixed
-  {
-    return $this->index;
-  }
+    public function current(): mixed
+    {
+        return $this->lines[$this->index];
+    }
 
-  public function valid(): bool
-  {
-    return $this->index < count($this->lines);
-  }
+    public function next(): void
+    {
+        $this->index++;
+    }
 
-  public function rewind(): void
-  {
-    $this->index = 0;
-  }
+    public function key(): mixed
+    {
+        return $this->index;
+    }
+
+    public function valid(): bool
+    {
+        return $this->index < count($this->lines);
+    }
+
+    public function rewind(): void
+    {
+        $this->index = 0;
+    }
 }
