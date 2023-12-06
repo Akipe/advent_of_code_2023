b10e54de5b7117c469a7d6023a317dc29153cd72
Julien Milletre Akipe <code.julien@milletre.fr>
2023-12-06 21:44:25 +0100

    Typescript: remove unused code & refactor

--- typescript/src/Utils/file.ts
+++ typescript/src/Utils/file.ts
@@ -1,12 +1,9 @@
 import * as fs from 'node:fs';
 
-const inputPathExample = "statement/Day05Part1/input_example.txt"
 const returnLineRegex = /\r?\n/
 
-const fileContent = fs.readFileSync(inputPathExample, 'utf-8')
-
 export const getLinesContentFile = (path: string): string[] => {
-    const fileContent = fs.readFileSync(path, "utf-8")
-
-    return fileContent.split(returnLineRegex)
+    return fs
+        .readFileSync(path, "utf-8")
+        .split(returnLineRegex)
 }
